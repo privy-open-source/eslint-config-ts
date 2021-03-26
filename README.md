6fe166472c20f2a98c42238c275083b548daf571
Ade Novid <adenvt@gmail.com>
2021-03-26 10:48:25 +0700

    added: initial config

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-eslint-config-ts
+# @privyid/eslint-config-ts
+> Eslint shareable config for Typescript
+
+## Usage
+
+First, install all dependencies
+
+### NPM
+```
+npm install --save-dev \
+  eslint \
+  babel-eslint \
+  eslint-config-standard \
+  eslint-plugin-import \
+  eslint-plugin-node \
+  eslint-plugin-promise \
+  eslint-plugin-standard \
+  eslint-plugin-unicorn \
+  eslint-plugin-varspacing \
+  eslint-plugin-vue \
+  eslint-config-adenvt \
+  @typescript-eslint/eslint-plugin \
+  @typescript-eslint/parser
+```
+### Yarn
+```
+yarn add --dev \
+  eslint \
+  babel-eslint \
+  eslint-config-standard \
+  eslint-plugin-import \
+  eslint-plugin-node \
+  eslint-plugin-promise \
+  eslint-plugin-standard \
+  eslint-plugin-unicorn \
+  eslint-plugin-varspacing \
+  eslint-plugin-vue \
+  eslint-config-adenvt \
+  @typescript-eslint/eslint-plugin \
+  @typescript-eslint/parser
+```
+
+
+Then, add this to your `.eslintrc` file (create one if doesn't exist):
+
+```json
+{
+  "extends": "@privyid/eslint-config-ts"
+}
+```
+
+Optional, add this to `package.json` script's
+```json
+{
+  // ...
+  "scripts": {
+    // ...
+    "lint:js": "eslint --ext .ts,.js,.vue --ignore-path .gitignore .",
+    "fix:js": "eslint --fix --ext .ts,.js,.vue --ignore-path .gitignore .",
+    // ...
+  }
+  // ...
+}
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ index.js
@@ -0,0 +1,87 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'adenvt',
+  ],
+  plugins      : ['@typescript-eslint'],
+  parserOptions: {
+    parser    : '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-multi-spaces': [
+      'error',
+      {
+        exceptions: {
+          VariableDeclarator       : true,
+          AssignmentExpression     : true,
+          ClassProperty            : true,
+          TSImportEqualsDeclaration: true,
+          Program                  : true,
+        },
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          multiline    : true,
+          minProperties: 3,
+        },
+        ExportDeclaration: 'always',
+        ImportDeclaration: {
+          multiline    : true,
+          minProperties: 3,
+        },
+        ObjectPattern: {
+          multiline    : true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'camelcase'                                       : 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define'         : 'off',
+    'unicorn/prevent-abbreviations'                   : ['error', { whitelist: { props: true } }],
+  },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires'               : 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        'no-unused-vars'                    : 'off',
+        '@typescript-eslint/no-unused-vars' : ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/ban-ts-ignore'  : 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+    {
+      files: ['**/*.vtpw.ts'],
+      rules: {
+        'unicorn/filename-case': 'off',
+        'spaced-comment'       : 'off',
+      },
+    },
+    {
+      files: ['src/pages/**/*.vue', 'src/layouts/**/*.vue'],
+      rules: { 'unicorn/filename-case': ['error', { case: 'kebabCase' }] },
+    },
+    {
+      files  : ['**/*.spec.js', '**/*.spec.ts'],
+      env    : { jest: true, node: true },
+      globals: {
+        page         : true,
+        browser      : true,
+        context      : true,
+        jestPuppeteer: true,
+      },
+    },
+  ],
+}
